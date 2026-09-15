9051259e09ae6cc4080b42fd0b946b883f8b6d7d
Mutma-Innah-learn <mutmamagaji8@gmail.com>
2026-09-15 09:31:24 +0100

    Implement TxMonitor2 contract for transaction monitoring
    This contract monitors transactions, flags suspicious ones, and records all transactions.

---
+++ txMonitor.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier
+
+pragma solidity ^0.8.0;
+
+contract TxMonitor2 {
+
+    event TransactionRecorded(address indexed sender, address indexed recipient, uint256 amount, bool flagged);
+    event TransactionFlagged(address indexed sender, address indexed recipient, uint256 amount);
+
+    struct Transaction {
+        address sender;
+        address recipient;
+        uint256 amount;
+        uint256 timestamp;
+        bool flagged;
+    }
+
+    uint256 public totalTrans;
+    uint256 public totalAmountTrans;
+    uint256 public suspiciousThreshold = 30;
+    uint256 public flaggedCount;
+    uint256 public successfulCount;
+
+    mapping(address => uint256) public balance;
+    Transaction[] public transactions;
+
+    function deposit(address me) public payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function transaction(address _recipient) public payable{
+        require(msg.value > 0, "Invalid amount");
+        require(msg.sender != _recipient, "Cannot send to yourself");
+        require(balance[msg.sender] > msg.value, "Insufficient funds");
+      
+         bool isFlagged = msg.value >= suspiciousThreshold;
+
+         //require(!isFlagged,"Suspicious");
+         
+        if (isFlagged) {
+            flaggedCount++;
+            emit TransactionFlagged(msg.sender, _recipient, msg.value);
+        } else {
+            successfulCount++;
+        }
+
+       
+      (bool success,) = payable(_recipient).call{value: msg.value}("");
+        require(success, "Transfer failed");
+
+        balance[msg.sender] -= msg.value;
+        balance[_recipient] += msg.value;
+        
+        totalTrans++;
+        totalAmountTrans += msg.value;
+
+        transactions.push(Transaction({
+            sender: msg.sender,
+            recipient: _recipient,
+            amount: msg.value,
+            timestamp: block.timestamp,
+            flagged: !isFlagged
+        }));
+
+        emit TransactionRecorded(msg.sender, _recipient, msg.value,!isFlagged);
+    }
+
+    function getTransaction() public view returns (Transaction[] memory) {
+        return transactions;
+    }
+}
